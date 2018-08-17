87e00ed0420ff905e2ff47daf3c03088a177a2de
macksas <metalksas@hotmail.com>
2018-08-17 17:39:13 -0500

    Initial commit

---
+++ weather-app/src/components/Location.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Location = () => (
+<div>
+<h1>
+    Bogota
+</h1>
+
+</div>
+
+);
+
+export default Location; 
---
+++ weather-app/src/components/WeatherLocation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Location from './Location';
+
+
+const WeatherLocation = () => (
+    <div>
+        <h1>Rain</h1>
+        <Location></Location>
+
+    </div>
+);
+
+
+export default WeatherLocation;
